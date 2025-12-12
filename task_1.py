5811a7386f7bb7fed5c327b3e76ed1f8d1a068c6
Nata19pv51 <nidnatasha@gmail.com>
2025-12-12 15:02:11 +0000

    Added readme.md file

--- task_1.py
+++ task_1.py
@@ -27,64 +27,23 @@ def show_graph(graph, property, start_node):
 
     plt.show()
 
-
-# def dijkstra(graph, start):
-#     # Ініціалізація відстаней та множини невідвіданих вершин
-#     distances_between_city = {vertex: float('infinity') for vertex in graph}
-#     distances_between_city[start] = 0
-#     unvisited = set(graph.nodes())
-#     # unvisited = list(graph.keys())
-
-#     while unvisited:
-#         # Знаходження вершини з найменшою відстанню серед невідвіданих
-#         current_vertex = min(unvisited, key=lambda vertex: distances_between_city[vertex])
-
-#         # Якщо поточна відстань є нескінченністю, то ми завершили роботу
-#         if distances_between_city[current_vertex] == float('infinity'):
-#             break
-        
-#         for neighbor, attributes in graph.adj[current_vertex].items():
-#             if "distance" not in attributes:
-#                 continue
-                
-#             weight = attributes["distance"]
-#             new_distance = distances_between_city[current_vertex] + weight 
-
-#             # Якщо нова відстань коротша, то оновлюємо найкоротший шлях
-#             if new_distance < distances_between_city[neighbor]:
-#                 distances_between_city[neighbor] = new_distance
-
-#         # Видаляємо поточну вершину з множини невідвіданих
-#         unvisited.remove(current_vertex)
-
-#     return distances_between_city
-
 def init_graph():
     transport_graph = nx.DiGraph()
-    transport_graph.add_edge("Київ", "Глеваха", distance=34, time=49)
-    transport_graph.add_edge("Васильків", "Гребенки", distance=30, time=26)
-    transport_graph.add_edge("Київ", "Васильків", distance=50, time=50)
-    transport_graph.add_edge("Київ", "Обухів", distance=47, time=44)
-    transport_graph.add_edge("Глеваха", "Фастів", distance=48, time=42)
-    transport_graph.add_edge("Обухів", "Кагарлик", distance=33, time=27)
-    transport_graph.add_edge("Кагарлик", "Біла Церква", distance=58, time=55)
-    transport_graph.add_edge("Фастів", "Біла Церква", distance=38, time=40)
-    transport_graph.add_edge("Глеваха", "Біла Церква", distance=59, time=47)
-    transport_graph.add_edge("Гребенки", "Біла Церква", distance=20, time=20)
+    transport_graph.add_edge("Київ", "Глеваха", distance=34)
+    transport_graph.add_edge("Васильків", "Гребенки", distance=30)
+    transport_graph.add_edge("Київ", "Васильків", distance=50)
+    transport_graph.add_edge("Київ", "Обухів", distance=47)
+    transport_graph.add_edge("Глеваха", "Фастів", distance=48)
+    transport_graph.add_edge("Обухів", "Кагарлик", distance=33)
+    transport_graph.add_edge("Кагарлик", "Біла Церква", distance=58)
+    transport_graph.add_edge("Фастів", "Біла Церква", distance=38)
+    transport_graph.add_edge("Глеваха", "Біла Церква", distance=59)
+    transport_graph.add_edge("Гребенки", "Біла Церква", distance=20)
     
     return transport_graph
 
 
 if __name__ == "__main__":
     transport_graph = init_graph()
-# print(transport_graph.adj["Київ"])
-# print(transport_graph.nodes())
-# print(transport_graph)
-# characteristics(transport_graph)
-
-# search_bfs_iterative(transport_graph, "Київ", "Біла Церква")
-# print("\n")
-# # search_bfs(transport_graph, deque(["Київ"]), "Біла Церква")
-# # print("\n")
 
     show_graph(transport_graph, "distance", 'Київ')
--- task_2.py
+++ task_2.py
@@ -4,24 +4,40 @@ from task_1 import init_graph, show_graph
 
 def search_dfs_iterative(graph, start_node, target_node=None):
     visited = set()
-    # Використовуємо стек для зберігання вершин
-    stack = [start_node]  
+    stack = [start_node]
+    # використовуємо словник для зберігання батьківського вузла
+    parent = {start_node: None} 
+
     while stack:
-        # Вилучаємо вершину зі стеку
-        vertex = stack.pop()
-        
-        if vertex == target_node:
-            print(vertex, end=' ')
-            visited.add(vertex)
-            return vertex
-              
-        if vertex not in visited:
-            print(vertex, end=' ')
-            # Відвідуємо вершину
-            visited.add(vertex)
-            # Додаємо сусідні вершини до стеку
-            stack.extend(reversed(list(graph[vertex]))) 
+        u = stack.pop() 
+
+        if u not in visited:
+            print(u, end=' ')
+            visited.add(u)
+
+            if u == target_node:
+                print("\n")
+                return _reconstruct_path(parent, target_node)
             
+            # використовуємо reversed, щоб дотримуватися порядку
+            for v in reversed(list(graph[u])):
+                if v not in visited:
+                    # додаємо сусідів до стеку
+                    stack.append(v)
+                    # встановлюємо, що u є батьком v
+                    parent[v] = u 
+                    
+    return None
+
+# Функція для відновлення шляху
+def _reconstruct_path(parent, target_node):
+    path = []
+    current = target_node
+    while current is not None:
+        path.append(current)
+        current = parent.get(current)
+    return path[::-1] # Повертаємо шлях у правильному порядку (від start до target)
+
 def search_bfs_iterative(graph, start_node, target_node=None):
     queue = deque([start_node])
     visited = {start_node}
@@ -40,24 +56,26 @@ def search_bfs_iterative(graph, start_node, target_node=None):
                 queue.append(v)
 
                 path[v] = path[u] + [v]
+    print("\n")
+    return path
 
 if __name__ == "__main__":
     graph = init_graph()
     
     print("************************************* BFS ************************************")
-    search_bfs_iterative(graph, "Київ")
+    print(search_bfs_iterative(graph, "Київ"))
     print("\n")
-    search_bfs_iterative(graph, "Київ", "Гребенки")
+    print(search_bfs_iterative(graph, "Київ", "Гребенки"))
     print("\n")
-    search_bfs_iterative(graph, "Київ", "Біла Церква")
+    print(search_bfs_iterative(graph, "Київ", "Біла Церква"))
     print("\nBFS - DONE")
     
     print("************************************* DFS ************************************")
-    search_dfs_iterative(graph, "Київ")
+    print(search_dfs_iterative(graph, "Київ"))
     print("\n")
-    search_dfs_iterative(graph, "Київ", "Гребенки")
+    print(search_dfs_iterative(graph, "Київ", "Гребенки"))
     print("\n")
-    search_dfs_iterative(graph, "Київ", "Біла Церква")
+    print(search_dfs_iterative(graph, "Київ", "Біла Церква"))
     print("\nDFS - DONE")
     
     show_graph(graph, "distance", "Київ")
